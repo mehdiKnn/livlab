18fb2a3de1bc4189c8a1b52702933e67d4ec800d
mehdiknn <mehdi@kannounnel.dev>
2022-05-17 20:26:15 +0300

    LivLav test

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
-# untitled1
+# Test technique LivLab
 
-## Project setup
-```
-yarn install
-```
+Faire un projet en Vue.js contenant 1 seule page avec :
 
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
+-   1 champ de recherche + son bouton de validation (bouton optionnel)
 
-### Compiles and minifies for production
-```
-yarn build
-```
+-   1 liste textuelle contenant les 10 sports des Jeux Olympiques suivants
+-   Décathlon (H)
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+-   110m haies (F)
+
+-   4x400m (F)
+
+-   Poids (H)
+
+-   France - Slovénie (Basket H)
+
+-   Boxe - moins de 60 kg (F)
+-   Canoë-kayak - K1 200m (H)
+
+-   Omnium (H)
+
+-   Keirin (F) Demi-finales
+
+-   Australie - États-Unis (Football F)
+
+
+La page doit afficher le champ de recherche + la liste complète des 10 sports.
+
+Le champ de recherche filtre quant à lui la liste dynamiquement et il doit être insensible à la casse et aux caractères spéciaux ou accentués.
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <input id="search" type="text" v-model="search" placeholder="Search">
+    <div v-if="filteredList.length > 0" class="sports" v-for="item in filteredList">
+      {{item}}
+    </div>
+    <img v-else id="travolta" src="https://www.hyperui.dev/photos/confused-travolta.gif" alt="John Travolta confused">
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      search: "",
+      list: [
+        "Décathlon (H)",
+        "110m haies (F)",
+        "4x400m (F)",
+        "Poids (H)",
+        "France - Slovénie (Basket H)",
+        "Boxe - moins de 60 kg (F)",
+        "Canoë-kayak - K1 200m (H)",
+        "Omnium (H)",
+        "Keirin (F) Demi-finales",
+        "Australie - États-Unis (Football F)",
+      ]
+    }
+  },
+  computed: {
+    filteredList: function () {
+      return this.list.filter(item => {
+       return this.cleanString(item).includes(this.cleanString(this.search))
+      })
+    }
+  },
+  methods:{
+    cleanString(str){
+      return str.normalize('NFD')
+          .replace(/([\u0300-\u036f]|[^\da-zA-Z])/g, '').toLowerCase()
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+
+body{
+  font-family: "Roboto Light",sans-serif;
+  margin: 0;
+}
+#container{
+  max-width: 1024px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#search{
+  font-family: inherit;
+  margin-top: 40px;
+  margin-bottom: 30px;
+  width: 50%;
+  padding: 1rem;
+  border: 1px solid white;
+  border-radius: 5px;
+  font-size: 15px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0 6px 24px 0, rgba(0, 0, 0, 0.08) 0 0 0 1px;
+  font-weight: lighter;
+}
+
+.sports{
+  font-size: 14px;
+  font-weight: lighter;
+  width: 40%;
+  padding: 0.8rem;
+  border: 1px solid white;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0 6px 24px 0, rgba(0, 0, 0, 0.08) 0 0 0 1px;
+  margin-bottom: 10px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+#travolta{
+  border-radius: 10px;
 }
 </style>
